389f3ca15904d00192e203fd23a696ce82952800
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2022-07-19 18:44:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240, 336);
+	canvas.parent('canvas1');
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+
+	posenet = ml5.poseNet(video, modelLoaded);
+	posenet.on('pose', gotResults);
+}
+
+function modelLoaded() {
+	console.log("Model has been loaded");
+}
+
+function gotResults(results) {
+	if (results.length > 0) {
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("nose X = " + noseX + "nose Y = " + noseY);
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
